fd74fed86ce2b9024c6fe8a81becd7ef92951808
Matt Keeler <mjkeeler7@gmail.com>
2021-02-22 14:15:07 -0500

    Add namespaced token support to the consul_acl_token_secret_id data source

--- consul/data_source_consul_acl_token_secret_id_test.go
+++ consul/data_source_consul_acl_token_secret_id_test.go
@@ -26,6 +26,37 @@ func TestAccDataACLTokenSecretID_basic(t *testing.T) {
 	})
 }
 
+func TestAccDataACLTokenSecretID_namespaceCE(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { skipTestOnConsulEnterpriseEdition(t) },
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataACLTokenSecretIDConfigNamespaceCE,
+				ExpectError: namespaceEnterpriseFeature,
+			},
+		},
+	})
+}
+
+func TestAccDataACLTokenSecretID_namespaceEE(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { skipTestOnConsulCommunityEdition(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataACLTokenSecretIDConfigNamespaceEE,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.consul_acl_token_secret_id.read", "pgp_key", ""),
+					resource.TestCheckResourceAttr("data.consul_acl_token_secret_id.read", "encrypted_secret_id", ""),
+					resource.TestCheckResourceAttr("data.consul_acl_token_secret_id.read", "namespace", "test-data-token-secret"),
+					testAccCheckTokenExistsAndValidUUID("data.consul_acl_token_secret_id.read", "secret_id"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataACLTokenSecretID_PGP(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
@@ -92,5 +123,37 @@ resource "consul_acl_token" "test" {
 data "consul_acl_token_secret_id" "read" {
 	accessor_id = "${consul_acl_token.test.id}"
 	pgp_key     = "keybase:terraformacctest"
+}
+`
+
+const testAccDataACLTokenSecretIDConfigNamespaceCE = `
+data "consul_acl_token" "read" {
+  accessor_id = "foo"
+  namespace   = "test-data-token"
+}
+`
+
+const testAccDataACLTokenSecretIDConfigNamespaceEE = `
+resource "consul_namespace" "test" {
+  name = "test-data-token-secret"
+}
+
+resource "consul_acl_policy" "test" {
+  name        = "test"
+  rules       = "node \"\" { policy = \"read\" }"
+  datacenters = [ "dc1" ]
+  namespace   = consul_namespace.test.name
+}
+
+resource "consul_acl_token" "test" {
+  description = "test"
+  policies    = ["${consul_acl_policy.test.name}"]
+  local       = true
+  namespace   = consul_namespace.test.name
+}
+
+data "consul_acl_token_secret_id" "read" {
+  accessor_id = consul_acl_token.test.id
+  namespace   = consul_namespace.test.name
 }
 `
