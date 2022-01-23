2cee1ffba85ea6f7b38592e484beeac9cf98abc3
Rémi Lapeyre <remi.lapeyre@lenstra.fr>
2022-01-24 00:34:17 +0100

    Add deprecation message to consul_license

--- consul/resource_consul_license_test.go
+++ consul/resource_consul_license_test.go
@@ -1,33 +1,14 @@
 package consul
 
 import (
-	"os"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccConsulLicense_FailOnCommunityEdition(t *testing.T) {
+func TestAccConsulLicense(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			skipTestOnConsulEnterpriseEdition(t)
-			skipTestForVersionsAfter(t, "1.10")
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccConsulLicense,
-				// Setting the Consul license will fail on the Community Edition
-				ExpectError: regexp.MustCompile("failed to set license: Unexpected response code: 404"),
-			},
-		},
-	})
-}
-
-func TestAccConsulLicense_BadLicense(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { skipTestOnConsulCommunityEdition(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
@@ -38,36 +19,8 @@ func TestAccConsulLicense_BadLicense(t *testing.T) {
 	})
 }
 
-func TestAccConsulLicense_CorrectLicense(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			skipTestOnConsulCommunityEdition(t)
-			if _, err := os.Stat("../test_license.hclic"); os.IsNotExist(err) {
-				t.Skip("This test needs a valid 'test_license.hclic' file to run.")
-			}
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccConsulLicense_CorrectLicense,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("consul_license.license", "valid", "true"),
-					resource.TestCheckResourceAttr("consul_license.license", "product", "consul"),
-					resource.TestCheckResourceAttr("consul_license.license", "warnings.#", "0"),
-				),
-			},
-		},
-	})
-}
-
 const testAccConsulLicense = `
 resource "consul_license" "license" {
 	license = "foobar"
-}
-`
-
-const testAccConsulLicense_CorrectLicense = `
-resource "consul_license" "license" {
-	license = file("../test_license.hclic")
 }
 `
