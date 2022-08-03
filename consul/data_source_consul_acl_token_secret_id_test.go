3a6c9be52643586f7aee8d6d421085882254c237
Matt Keeler <mjkeeler7@gmail.com>
2022-08-03 14:27:57 -0400

    Add support for grabbing the secret id from a partitioned token.

--- consul/data_source_consul_acl_token_secret_id_test.go
+++ consul/data_source_consul_acl_token_secret_id_test.go
@@ -62,6 +62,41 @@ func TestAccDataACLTokenSecretID_namespaceEE(t *testing.T) {
 	})
 }
 
+func TestAccDataACLTokenSecretID_partitionCE(t *testing.T) {
+	providers, _ := startTestServer(t)
+
+	resource.Test(t, resource.TestCase{
+		Providers: providers,
+		PreCheck:  func() { skipTestOnConsulEnterpriseEdition(t) },
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDataACLTokenSecretIDConfigPartitionCE,
+				ExpectError: regexp.MustCompile("(?i)Consul Enterprise feature"),
+			},
+		},
+	})
+}
+
+func TestAccDataACLTokenSecretID_partitionEE(t *testing.T) {
+	providers, _ := startTestServer(t)
+
+	resource.Test(t, resource.TestCase{
+		Providers: providers,
+		PreCheck:  func() { skipTestOnConsulCommunityEdition(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataACLTokenSecretIDConfigPartitionEE,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.consul_acl_token_secret_id.read", "pgp_key", ""),
+					resource.TestCheckResourceAttr("data.consul_acl_token_secret_id.read", "encrypted_secret_id", ""),
+					resource.TestCheckResourceAttr("data.consul_acl_token_secret_id.read", "partition", "test-data-token-secret"),
+					testAccCheckTokenExistsAndValidUUID("data.consul_acl_token_secret_id.read", "secret_id"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataACLTokenSecretID_PGP(t *testing.T) {
 	providers, _ := startTestServer(t)
 
@@ -161,5 +196,37 @@ resource "consul_acl_token" "test" {
 data "consul_acl_token_secret_id" "read" {
   accessor_id = consul_acl_token.test.id
   namespace   = consul_namespace.test.name
+}
+`
+
+const testAccDataACLTokenSecretIDConfigPartitionCE = `
+data "consul_acl_token" "read" {
+  accessor_id = "foo"
+  partition   = "test-data-token"
+}
+`
+
+const testAccDataACLTokenSecretIDConfigPartitionEE = `
+resource "consul_admin_partition" "test" {
+  name = "test-data-token-secret"
+}
+
+resource "consul_acl_policy" "test" {
+  name        = "test-data-token-secret"
+  rules       = "node \"\" { policy = \"read\" }"
+  datacenters = [ "dc1" ]
+  partition   = consul_admin_partition.test.name
+}
+
+resource "consul_acl_token" "test" {
+  description = "test"
+  policies    = [consul_acl_policy.test.name]
+  local       = true
+  partition   = consul_admin_partition.test.name
+}
+
+data "consul_acl_token_secret_id" "read" {
+  accessor_id = consul_acl_token.test.id
+  partition   = consul_admin_partition.test.name
 }
 `
