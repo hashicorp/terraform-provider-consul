83443fb381c4c00be580497b09ec7b9ed985b3e2
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-09-30 17:23:11 +0530

    server resolver tests flaky

--- consul/resource_consul_config_entry_service_resolver_ce_test.go
+++ consul/resource_consul_config_entry_service_resolver_ce_test.go
@@ -16,66 +16,153 @@ func TestAccConsulConfigEntryServiceResolverCETest(t *testing.T) {
 		PreCheck:  func() { skipTestOnConsulEnterpriseEdition(t) },
 		Providers: providers,
 		Steps: []resource.TestStep{
+			//{
+			//	Config: testConsulConfigEntryServiceResolverCEWithRedirect,
+			//	Check: resource.ComposeTestCheckFunc(
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "name", "consul-service-resolver-1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "meta.key", "value"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "connect_timeout", "10s"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "request_timeout", "10s"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.#", "2"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.0.name", "v1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.0.filter", "Service.Meta.version == v1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.0.only_passing", "true"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.1.name", "v2"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.1.filter", "Service.Meta.version == v2"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "subsets.1.only_passing", "true"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "default_subset", "v1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "redirect.#", "1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "redirect.1000671749.datacenter", "dc1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.policy", "ring_hash"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.ring_hash_config.#", "1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.ring_hash_config.1100849243.minimum_ring_size", "3"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.ring_hash_config.1100849243.maximum_ring_size", "10"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.hash_policies.#", "1"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.hash_policies.0.field", "header"),
+			//		resource.TestCheckResourceAttr("consul_config_entry_service_resolver.foo", "load_balancer.1867531597.hash_policies.0.field_value", "x-user-id"),
+			//	),
+			//},
 			{
-				Config: testConsulConfigEntryServiceResolverCE,
-				Check:  resource.ComposeTestCheckFunc(),
+				Config: testConsulConfigEntryServiceResolverCEWithFailover,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "name", "consul-service-resolver-2"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "meta.key", "value"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "connect_timeout", "10s"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "request_timeout", "10s"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.#", "2"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.0.name", "v1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.0.filter", "Service.Meta.version == v1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.0.only_passing", "true"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.1.name", "v2"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.1.filter", "Service.Meta.version == v2"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "subsets.1.only_passing", "true"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "default_subset", "v1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.#", "3"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.0.subset_name", "*"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.0.service", "backend"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.0.datacenters", "[\"dc3\", \"dc4\"]"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.1.subset_name", "v2"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.1.service", "frontend"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.1.datacenters", "[\"dc2\"]"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.1.subset_name", "v1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.2.targets.#", "1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.2.targets.0.service_subset", "v1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.2.targets.0.datacenter", "dc1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "failover.2.datacenters", "[\"dc2\"]"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.policy", "ring_hash"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.ring_hash_config.#", "1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.ring_hash_config.1100849243.minimum_ring_size", "3"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.ring_hash_config.1100849243.maximum_ring_size", "10"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.hash_policies.#", "1"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.hash_policies.0.field", "header"),
+					resource.TestCheckResourceAttr("consul_config_entry_service_resolver.bar", "load_balancer.1867531597.hash_policies.0.field_value", "x-user-id"),
+				),
 			},
 		},
 	})
 }
 
-const testConsulConfigEntryServiceResolverCE = `
-resource "consul_config_entry" "service_resolver" {
-	kind = "service-resolver"
-	name = consul_config_entry.web.name
-
-	config_json = jsonencode({
-		DefaultSubset = "v1"
-
-		Subsets = {
-			"v1" = {
-				Filter = "Service.Meta.version == v1"
-			}
-			"v2" = {
-				Filter = "Service.Meta.version == v2"
-			}
-		}
-	})
-}
-
-resource "consul_config_entry_service_splitter" "foo" {
-	name = consul_config_entry.service_resolver.name
+const testConsulConfigEntryServiceResolverCEWithRedirect = `
+resource "consul_config_entry_service_resolver" "foo" {
+	name = "consul-service-resolver-1"
 	meta = {
-		key = "value"
+		key: "value"
 	}
-	splits {
-		weight  = 90
-		service_subset  = "v1"
-		service = "web"
-		request_headers {
-			set = {
-				"x-web-version": "from-v1"
-			}
+	connect_timeout = "10s"
+	request_timeout = "10s"
+	subsets {
+		name = "v1"
+		filter = "Service.Meta.version == v1"
+		only_passing = true
+	}
+	subsets {
+		name = "v2"
+		filter = "Service.Meta.version == v2"
+		only_passing = true
+	}
+	default_subset = "v1"
+	redirect {
+		datacenter = "dc1"
+	}
+	load_balancer {
+		policy = "ring_hash"
+		ring_hash_config {
+			minimum_ring_size = 3
+			maximum_ring_size = 10
 		}
-		response_headers {
-			set = {
-				"x-web-version": "to-v1"
-			}
+		hash_policies {
+			field = "header"
+			field_value = "x-user-id"
 		}
 	}
-	splits {
-		weight  = 10
-		service = "web"
-		service_subset  = "v2"
-		request_headers {
-			set = {
-				"x-web-version": "from-v2"
-			}
+}
+`
+
+const testConsulConfigEntryServiceResolverCEWithFailover = `
+resource "consul_config_entry_service_resolver" "bar" {
+	name = "consul-service-resolver-2"
+	meta = {
+		key: "value"
+	}
+	connect_timeout = "10s"
+	request_timeout = "10s"
+	subsets {
+		name = "v1"
+		filter = "Service.Meta.version == v1"
+		only_passing = true
+	}
+	subsets {
+		name = "v2"
+		filter = "Service.Meta.version == v2"
+		only_passing = true
+	}
+	default_subset = "v1"
+	failover {
+		subset_name  = "*"
+		service      = "backend"
+		datacenters = ["dc3", "dc4"]
+	}
+	failover {
+		subset_name = "v2"
+		service = "frontend"
+		datacenters = ["dc2"]
+	}
+	failover {
+		subset_name = "v1"
+		targets {
+			service_subset = "v1"
+			datacenter = "dc1"
 		}
-		response_headers {
-			set = {
-				"x-web-version": "to-v2"
-			}
+	}
+	load_balancer {
+		policy = "ring_hash"
+		ring_hash_config {
+			minimum_ring_size = 3
+			maximum_ring_size = 10
+		}
+		hash_policies {
+			field = "header"
+			field_value = "x-user-id"
 		}
 	}
 }
